62889005dbbc6e5a1a6df0a2b085e80297adfd03
Jaemin Choi <1dotolee@gmail.com>
2021-10-01 00:05:00 +0000

    fix: Nginx and PM2 conflicts

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,21 +1,15 @@
 module.exports = {
   apps: [
     {
-      name: 'proxy',
-      script: 'nginx',
-      args: '-c ./nginx.conf',
-      error_file: '$LOG_PATH/nginx_err.log',
-      out_file: '$LOG_PATH/nginx.log'
-    },
-    {
       name: 'backend',
       script: 'yarn',
-      args: '--cwd ./backend dev',
+      cwd: '/app/backend',
+      args: 'dev',
       autorestart: true,
       watch: false,
-      instances: 0,
-      error_file: '$LOG_PATH/backend_err.log',
-      out_file: '$LOG_PATH/backend.log',
+      error_file: '/data/log/backend_err.log',
+      out_file: '/data/log/backend.log',
+      interpreter: '/bin/sh',
       env_production: {
         NODE_ENV: 'production'
       },
@@ -26,19 +20,18 @@ module.exports = {
     {
       name: 'frontend',
       script: 'yarn',
-      args: '--cwd ./frontend start',
+      cwd: '/app/frontend',
+      args: 'start',
       autorestart: true,
       watch: false,
-      instances: 0,
-      error_file: '$LOG_PATH/frontend_err.log',
-      out_file: '$LOG_PATH/frontend.log',
+      error_file: '/data/log/frontend_err.log',
+      out_file: '/data/log/frontend.log',
+      interpreter: '/bin/sh',
       env_production: {
-        NODE_ENV: 'production',
-        LOG_PATH: '/data/log'
+        NODE_ENV: 'production'
       },
       env_development: {
-        NODE_ENV: 'development',
-        LOG_PATH: './log'
+        NODE_ENV: 'development'
       }
     }
   ],
