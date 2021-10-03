ec34cbb65e5defc98bc29e21f2a7dc5b346123de
Kangmin Seo <kangrnin@gmail.com>
2021-10-03 10:57:42 +0000

    Add default layout components
    - Header, Footer
    - 하이퍼링크 주소나 드롭다운 기능은 미구현 상태

---
+++ frontend/components/Header.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="px-6 py-4 w-full flex justify-between items-center bg-white">
+    <div class="ml-20 flex">
+      <div class="flex items-center">
+        <img src="../img/logo.svg" alt="logo" class="w-10 h-10 inline-block" />
+        <span class="ml-3 text-gray-800 text-2xl font-bold">군in</span>
+      </div>
+      <div class="ml-12 flex items-center">
+        <NuxtLink to="/about" class="">서비스 소개</NuxtLink>
+        <NuxtLink to="/help" class="ml-10">도움말</NuxtLink>
+        <!-- TODO: 드롭다운으로 바꾸기 -->
+        <NuxtLink to="/help" class="ml-10">커뮤니티</NuxtLink>
+      </div>
+    </div>
+    <div class="mr-20">
+      <button class="text-gray-800">회원가입</button>
+      <button
+        class="
+          ml-8
+          px-6
+          py-1
+          text-gray-800 text-sm
+          border-2 border-gray-800
+          rounded-lg
+        "
+      >
+        로그인
+      </button>
+    </div>
+  </div>
+</template>
---
+++ frontend/layouts/default.vue
@@ -0,0 +1,7 @@
+<template>
+  <div class="w-full">
+    <Header />
+    <Nuxt />
+    <Footer />
+  </div>
+</template>
