de9bc8e6b1b0a4848aa9e98daf7211500927f174
Jaemin Choi <1dotolee@gmail.com>
2021-10-03 03:10:19 +0000

    Implment Login page for prototyping

---
+++ frontend/components/Login.vue
@@ -0,0 +1,79 @@
+<template>
+  <div class="h-screen flex flex-col items-center justify-center space-y-4">
+    <div class="flex flex-row items-end w-80 justify-between">
+      <Logo height="60px" width="60px" />
+      <span class="text-2xl text-gray-600 font-bold">군in 시작하기</span>
+    </div>
+    <input
+      v-model="form.username"
+      type="text"
+      class="
+        w-80
+        h-8
+        bg-gray-200
+        rounded-md
+        px-3
+        focus:ring-2 focus:ring-red-500
+      "
+      placeholder="아이디"
+      required
+    />
+    <input
+      v-model="form.password"
+      type="password"
+      class="
+        w-80
+        h-8
+        bg-gray-200
+        rounded-md
+        px-3
+        focus:ring-2 focus:ring-red-500
+      "
+      placeholder="비밀번호"
+      required
+    />
+    <button
+      class="
+        w-80
+        h-8
+        bg-military
+        text-white
+        rounded-md
+        font-bold
+        hover:opacity-80
+      "
+      @click="login"
+    >
+      로그인
+    </button>
+  </div>
+</template>
+
+<script>
+import Logo from './Logo.vue'
+
+export default {
+  components: {
+    Logo
+  },
+  data() {
+    return {
+      form: {
+        username: '',
+        password: ''
+      }
+    }
+  },
+  method: {
+    async login() {
+      const res = await this.$axios({
+        method: 'POST',
+        url: '/user/login',
+        data: this.form
+      })
+
+      res.status === 200 ? alert('Login succeed.') : alert(res.data)
+    }
+  }
+}
+</script>
---
+++ frontend/tailwind.config.js
@@ -0,0 +1,30 @@
+module.exports = {
+  theme: {
+    fontFamily: {
+      sans: [
+        'Pretendard',
+        '-apple-system',
+        'BlinkMacSystemFont',
+        'system-ui',
+        'Roboto',
+        'Helvetica Neue',
+        'Segoe UI',
+        'Apple SD Gothic Neo',
+        'Noto Sans KR',
+        'Malgun Gothic',
+        'sans-serif'
+      ]
+    },
+    extend: {
+      colors: {
+        military: '#4E784D'
+      }
+    }
+  },
+  variants: {
+    extend: {
+      ringColor: ['focus'],
+      ringWidth: ['focus']
+    }
+  }
+}
