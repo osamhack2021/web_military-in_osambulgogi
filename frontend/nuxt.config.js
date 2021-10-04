01f8259026acb130e2e5a72e99a0ef12fad6b353
Jaemin Choi <1dotolee@gmail.com>
2021-10-04 11:27:34 +0000

    Add vue-notification library

--- frontend/nuxt.config.js
+++ frontend/nuxt.config.js
@@ -22,7 +22,10 @@ export default {
   css: [],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
-  plugins: [],
+  plugins: [
+    { src: '~/plugins/notifications-ssr', ssr: true },
+    { src: '~/plugins/notifications', ssr: false }
+  ],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
   components: true,
