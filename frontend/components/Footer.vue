e12fd8c20acd77ee609333b244371678b9b5e80f
Kangmin Seo <kangrnin@gmail.com>
2021-10-04 07:23:30 +0000

    Edit Footer text content, size

--- frontend/components/Footer.vue
+++ frontend/components/Footer.vue
@@ -10,16 +10,17 @@
       justify-between
       items-center
       bg-white
+      text-sm
     "
   >
     <div class="flex items-center">
-      <span class="text-gray-300 font-bold">OSAMHACK2021, MIT LICENSE</span>
+      <span class="text-gray-400 font-bold">OSAMHACK2021, MIT LICENSE</span>
     </div>
-    <div class="">
-      <NuxtLink to="/about" class="">서비스 소개</NuxtLink>
-      <NuxtLink to="/help" class="ml-10">도움말</NuxtLink>
+    <div class="text-gray-400">
+      <NuxtLink to="/about">이용약관</NuxtLink>
+      <NuxtLink to="/help" class="ml-10">개인정보처리방침</NuxtLink>
       <!-- TODO: 드롭다운으로 바꾸기 -->
-      <NuxtLink to="/help" class="ml-10">커뮤니티</NuxtLink>
+      <NuxtLink to="/help" class="ml-10">Github</NuxtLink>
     </div>
   </div>
 </template>
