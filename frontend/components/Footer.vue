c19c1445aeaeb515546f993eccb2c5d088931bba
Kangmin Seo <kangrnin@gmail.com>
2021-10-04 08:05:31 +0000

    Change Header, Footer font color
    - 조금 더 연하게 수정
    - Footer 모든 글씨에 bold 적용

--- frontend/components/Footer.vue
+++ frontend/components/Footer.vue
@@ -10,13 +10,14 @@
       justify-between
       items-center
       bg-white
-      text-sm
+      text-gray-300 text-sm
+      font-bold
     "
   >
     <div class="flex items-center">
-      <span class="text-gray-400 font-bold">OSAMHACK2021, MIT LICENSE</span>
+      <span>OSAMHACK2021, MIT LICENSE</span>
     </div>
-    <div class="text-gray-400">
+    <div>
       <NuxtLink to="/about">이용약관</NuxtLink>
       <NuxtLink to="/help" class="ml-10">개인정보처리방침</NuxtLink>
       <!-- TODO: 드롭다운으로 바꾸기 -->
