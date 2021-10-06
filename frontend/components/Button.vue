2c397b42bd1c1fc6faa82badef0b6793a8cbedd5
Kangmin Seo <kangrnin@gmail.com>
2021-10-05 22:34:11 -0900

    Add Button component
    - 예약 추가, 당직표 수정, 새 글 작성 등 메인 기능에 공통적으로 사용되는 버튼
    - 일반 <button> 태그처럼 사용할 수 있음

---
+++ frontend/components/Button.vue
@@ -0,0 +1,16 @@
+<template>
+  <button
+    class="
+      px-3.5
+      py-1.5
+      text-white text-lg
+      font-bold
+      bg-military
+      rounded-lg
+      filter
+      drop-shadow
+    "
+  >
+    <slot />
+  </button>
+</template>
