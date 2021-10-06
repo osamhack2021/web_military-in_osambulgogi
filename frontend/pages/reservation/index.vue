74e1f43c348278da89bac4a4d0ad9dcbcb092af9
Kangmin Seo <kangrnin@gmail.com>
2021-10-05 22:16:06 -0900

    Fetch table on dropdown select in Reservation page

--- frontend/pages/reservation/index.vue
+++ frontend/pages/reservation/index.vue
@@ -29,29 +29,59 @@ export default {
   data() {
     return {
       facilities: [],
+      selectedFacility: '',
       columns: [],
       values: []
     }
   },
   async fetch() {
-    // this.values = await $axios.get('')
-    this.values = await new Promise((resolve) => resolve())
-
-    this.facilities = [
-      '사이버지식정보방1',
-      '사이버지식정보방2',
-      '사이버지식정보방3'
-    ]
-    this.columns = ['일자', '예약자', '좌석번호', '예약 시간', '비고']
-    this.values = [
-      ['9/1 수', '일병 서강민', '14번', '17:30 ~ 19:00', '군 e-러닝 중간고사'],
-      ['9/1 수', '일병 서강민', '14번', '17:30 ~ 19:00', '군 e-러닝 중간고사'],
-      ['9/1 수', '일병 서강민', '14번', '17:30 ~ 19:00', '군 e-러닝 중간고사'],
-      ['9/1 수', '일병 서강민', '14번', '17:30 ~ 19:00', '군 e-러닝 중간고사']
-    ]
+    await this.fetchFacility()
+    await this.fetchTable(this.facilities[0])
   },
   methods: {
-    onDropdownSelect(selectedOption) {}
+    async fetchFacility() {
+      // this.facilities = await $axios.get('')
+      this.facilities = await new Promise((resolve) => resolve())
+
+      this.facilities = [
+        '사이버지식정보방1',
+        '사이버지식정보방2',
+        '사이버지식정보방3'
+      ]
+      this.columns = ['일자', '예약자', '좌석번호', '예약 시간', '비고']
+    },
+    async fetchTable(facility) {
+      // this.values = await $axios.get('')
+      this.values = await new Promise((resolve) => resolve())
+
+      this.values = [
+        [
+          '9/1 수',
+          '일병 서강민',
+          '14번',
+          '17:30 ~ 19:00',
+          '군 e-러닝 중간고사'
+        ],
+        [
+          '9/1 수',
+          '일병 서강민',
+          '14번',
+          '17:30 ~ 19:00',
+          '군 e-러닝 중간고사'
+        ],
+        [
+          '9/1 수',
+          '일병 서강민',
+          '14번',
+          '17:30 ~ 19:00',
+          '군 e-러닝 중간고사'
+        ],
+        ['9/1 수', '일병 서강민', '14번', '17:30 ~ 19:00', '군 e-러닝 중간고사']
+      ]
+    },
+    onDropdownSelect(selectedOption) {
+      this.fetchTable(selectedOption)
+    }
   }
 }
 </script>
