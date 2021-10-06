cbe65885a424c0e14ecef04b0fdc522a30416a2a
Jaemin Choi <1dotolee@gmail.com>
2021-10-06 11:32:45 +0000

    fix: Request valid SSL certificate via openssl

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,7 +2,7 @@
 
 mkdir -p /data/log /data/ssl
 
-openssl req -new -newkey rsa:2048 -nodes -keyout /data/ssl/server.key -out /data/ssl/server.crt \
+openssl req -x509 -newkey rsa:2048 -nodes -keyout /data/ssl/server.key -out /data/ssl/server.crt \
  -subj /C=KR/ST=Seoul/L=Seoul/O=OSAMHACK2021/OU=OSAMBULGOGI/CN=military-in -days 1000
 
 cd /app/backend
