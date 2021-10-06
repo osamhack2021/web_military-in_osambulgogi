30d7e501c5c983c7aca1745a50209e3f9b3c4a5b
Kangmin Seo <kangrnin@gmail.com>
2021-10-05 21:51:35 -0900

    Add components to Reservation page
    - Dropdown, 예약 추가 button, Table

--- frontend/pages/reservation/index.vue
+++ frontend/pages/reservation/index.vue
@@ -1,5 +1,57 @@
 <template>
-  <div class="lg:w-2/3 m-auto">
-    <Table />
+  <div class="lg:w-3/4 m-auto">
+    <div class="flex justify-between">
+      <Dropdown :options="facilities" :on-select="onDropdownSelect" />
+      <button
+        class="
+          px-3.5
+          py-1.5
+          text-white text-lg
+          font-bold
+          bg-military
+          rounded-lg
+          filter
+          drop-shadow
+        "
+      >
+        <fa :icon="['fas', 'plus']" class="mr-1" />
+        예약 추가
+      </button>
+    </div>
+    <div class="mx-6 mt-14">
+      <Table :columns="columns" :values="values" />
+    </div>
   </div>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      facilities: [],
+      columns: [],
+      values: []
+    }
+  },
+  async fetch() {
+    // this.values = await $axios.get('')
+    this.values = await new Promise((resolve) => resolve())
+
+    this.facilities = [
+      '사이버지식정보방1',
+      '사이버지식정보방2',
+      '사이버지식정보방3'
+    ]
+    this.columns = ['일자', '예약자', '좌석번호', '예약 시간', '비고']
+    this.values = [
+      ['9/1 수', '일병 서강민', '14번', '17:30 ~ 19:00', '군 e-러닝 중간고사'],
+      ['9/1 수', '일병 서강민', '14번', '17:30 ~ 19:00', '군 e-러닝 중간고사'],
+      ['9/1 수', '일병 서강민', '14번', '17:30 ~ 19:00', '군 e-러닝 중간고사'],
+      ['9/1 수', '일병 서강민', '14번', '17:30 ~ 19:00', '군 e-러닝 중간고사']
+    ]
+  },
+  methods: {
+    onDropdownSelect(selectedOption) {}
+  }
+}
+</script>
