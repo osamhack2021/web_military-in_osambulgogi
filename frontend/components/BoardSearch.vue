d9e5a4627c5c954647c879c81d1360ad8c01b24f
juhy0987 <58243998+juhy0987@users.noreply.github.com>
2021-10-07 20:01:30 +0000

    Implement Search box for boards

---
+++ frontend/components/BoardSearch.vue
@@ -0,0 +1,51 @@
+<template>
+  <div
+    class="
+      flex flex-row
+      group
+      w-160
+      h-8
+      bg-gray-200
+      rounded-sm
+      items-center
+      justify-start
+      space-x-0.5
+    "
+  >
+    <template v-if="focused_search == 0">
+      <fa :icon="['fas', 'search']" class="ml-2 text-gray-400" />
+    </template>
+    <input
+      v-model="searchw"
+      v-focus="focused_search"
+      type="text"
+      class="
+        w-full
+        mr-2
+        border-transparent
+        bg-transparent
+        text-gray-600
+        placeholder-gray-400
+        items-center
+        justify-stretch
+        font-bold
+        focus:placeholder-transparent
+        mx-2
+      "
+      placeholder="검색"
+      @focus="focused_search = true"
+      @blur="focused_search = false"
+    />
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      searchw: '',
+      focused_search: false
+    }
+  }
+}
+</script>
