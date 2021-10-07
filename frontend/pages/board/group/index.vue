338f43d253f1365a8ffd2260fea1a20204eb8579
juhy0987 <58243998+juhy0987@users.noreply.github.com>
2021-10-07 20:04:49 +0000

    Add index file for group community

---
+++ frontend/pages/board/group/index.vue
@@ -0,0 +1,24 @@
+<template>
+  <div
+    class="flex items-start justify-center justfify-between my-12 space-x-12"
+  >
+    <div class="flex-none w-160 items-center justify-center space-y-8">
+      <BoardSearch />
+      <BoardBlock1 />
+      <BoardBlock1 />
+      <div class="flex flex-row items-stretch justify-between">
+        <BoardBlock2 />
+        <BoardBlock2 />
+      </div>
+      <div class="flex flex-row items-stretch justify-between">
+        <BoardBlock2 />
+        <BoardBlock2 />
+      </div>
+    </div>
+    <div class="flex flex-col items-center justify-center space-y-4">
+      <div class="w-48 h-36 text-center shadow-lg">top</div>
+      <div class="w-48 h-36 text-center shadow-lg">center</div>
+      <div class="w-48 h-36 text-center shadow-lg">bottom</div>
+    </div>
+  </div>
+</template>
