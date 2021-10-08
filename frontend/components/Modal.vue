dcc5f8e32026ace471a1f41837c34ea97d31bf85
Kangmin Seo <kangrnin@gmail.com>
2021-10-08 03:25:51 -0900

    Add Modal, ReservationModal
    - 다른 모달들도 Modal 컴포넌트를 이용한  커스텀 컴포넌트로 제작할 예정

---
+++ frontend/components/Modal.vue
@@ -0,0 +1,19 @@
+<template>
+  <div
+    class="
+      fixed
+      z-50
+      top-0
+      left-0
+      w-full
+      h-full
+      bg-gray-400 bg-opacity-50
+      flex
+      align-middle
+    "
+  >
+    <div class="sm:w-5/6 lg:w-1/4 m-auto p-8 rounded-2xl bg-white">
+      <slot />
+    </div>
+  </div>
+</template>
