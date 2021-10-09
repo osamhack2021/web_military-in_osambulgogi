511cb241ccde8d6330543577998a3a2e989fc2ba
Kangmin Seo <kangrnin@gmail.com>
2021-10-09 07:20:36 -0900

    Add RegisterModal for regsiter confirm
    - 모달 확인 후 리디렉션 구현 필요함

---
+++ frontend/components/RegisterModal.vue
@@ -0,0 +1,10 @@
+<template>
+  <Modal @close="$emit('close')">
+    <p class="text-xl text-center font-bold mb-3">
+      회원가입이 완료되었습니다.<br />이제 군in을 시작해보세요!
+    </p>
+    <Button class="w-full mt-4" @click="$emit('close')">
+      메인 페이지로 이동
+    </Button>
+  </Modal>
+</template>
