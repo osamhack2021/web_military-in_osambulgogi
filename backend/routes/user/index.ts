1713ca2fc302bfce16ff8230c972b5dd05e3febc
Jaemin Choi <1dotolee@gmail.com>
2021-10-11 11:20:03 +0000

    Implement register API

--- backend/routes/user/index.ts
+++ backend/routes/user/index.ts
@@ -1,6 +1,7 @@
 import * as login from './login'
 import * as logout from './logout'
 import * as profile from './profile'
+import * as register from './register'
 import { authMiddleware } from '../../middleware/auth'
 import { Router } from 'express'
 
@@ -10,5 +11,6 @@ router.post('/login', login.post)
 router.get('/logout', authMiddleware, logout.get)
 router.get('/profile', authMiddleware, profile.get)
 router.put('/profile', authMiddleware, profile.put)
+router.post('/register', register.post)
 
 export default router
