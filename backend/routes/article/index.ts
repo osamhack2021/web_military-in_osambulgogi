ecca618e914a7c97c152b0bc3028a1f8c79b7c28
Jaemin Choi <1dotolee@gmail.com>
2021-10-11 07:10:17 +0000

    Implement article voting API
    유저가 하나의 글에 중복에서 좋아요/싫어요 남길 수 없도록 처리한다.
    이미 좋아요가 남겨져 있는 글에 API 요청이 들어오면 좋아요를 취소한다.

--- backend/routes/article/index.ts
+++ backend/routes/article/index.ts
@@ -1,9 +1,11 @@
 import * as article from './article'
+import * as vote from './vote'
 import { authMiddleware } from '../../middleware/auth'
 import { Router } from 'express'
 
 const router = Router()
 
 router.get('/', authMiddleware, article.get)
+router.put('/vote', authMiddleware, vote.put)
 
 export default router
