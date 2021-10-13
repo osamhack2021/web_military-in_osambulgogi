9a3910c88d2b4449ee07111694c6c2f96ee2b9ef
juhy0987 <58243998+juhy0987@users.noreply.github.com>
2021-10-13 15:27:45 +0000

    Implement modifying mode and make variable intuitive

--- frontend/components/Article.vue
+++ frontend/components/Article.vue
@@ -11,7 +11,8 @@
     "
   >
     <div class="py-4 w-full">
-      <div
+      <NuxtLink
+        to="/board"
         class="
           flex flex-row
           w-full
@@ -19,72 +20,148 @@
           font-bold
           items-center
           gap-x-0.5
+          group
         "
       >
-        <fa :icon="['fas', 'chevron-left']" class="mr-1" />
-        <Nuxtlink to="/board/:id">자유 게시판</Nuxtlink>
-      </div>
+        <fa
+          :icon="['fas', 'chevron-left']"
+          class="group-hover:text-militaryDark group-hover:drop-shadow"
+        />
+        <div class="group-hover:text-militaryDark group-hover:drop-shadow">
+          {{ boardName }}
+        </div>
+      </NuxtLink>
       <div class="flex flex-row w-full items-center justify-between">
-        <span class="text-3xl text-gray-500 font-bold"
-          >군대에서 근무서면서 달을 보는데 정말 환하더군요</span
-        >
+        <span class="text-3xl text-gray-500 font-bold">{{ title }}</span>
         <div class="flex flex-col items-center space-y-1">
           <div class="flex flex-row items-center justify-between space-x-3">
-            <fa
-              :icon="['fas', 'caret-up']"
-              class="text-2xl text-gray-400 font-bold"
-            />
-            <span class="text-2xl text-gray-500">+45</span>
-            <fa
-              :icon="['fas', 'caret-down']"
-              class="text-2xl text-gray-400 font-bold"
-            />
+            <button class="flex items-center" @click="increaseUpVote()">
+              <fa
+                :icon="['fas', 'caret-up']"
+                class="
+                  text-2xl text-gray-400
+                  font-bold
+                  hover:drop-shadow hover:text-gray-600
+                "
+              />
+            </button>
+            <span class="text-2xl text-gray-500">
+              <template v-if="vote > 0"> +{{ vote }} </template>
+              <template v-else>{{ vote }}</template>
+            </span>
+            <button class="flex items-center" @click="increaseDownVote()">
+              <fa
+                :icon="['fas', 'caret-down']"
+                class="
+                  text-2xl text-gray-400
+                  font-bold
+                  hover:drop-shadow hover:text-gray-600
+                "
+              />
+            </button>
           </div>
-          <span class="text-xs text-gray-300">96%가 추천했습니다.</span>
+          <span class="text-xs text-gray-300"
+            >{{ ((upVote / (upVote + downVote)) * 100).toFixed(2) }}%가
+            추천했습니다.</span
+          >
         </div>
       </div>
-      <div class="flex flex-row w-full items-center gap-x-0.5">
+
+      <div class="flex flex-row items-center gap-x-0.5">
         <div class="flex w-8 h-8 mr-2 rounded-md bg-gray-300">
           <fa
             :icon="['fas', 'user']"
-            class="flex text-2xl m-auto w-full text-white bg-gray-300"
+            class="flex text-2xl m-auto text-white bg-gray-300"
           />
         </div>
-        <div class="flex flex-col w-full">
-          <span class="text-sm text-gray-500 font-bold">익명</span>
+        <div class="flex flex-col mr-8">
+          <span class="text-sm text-gray-500 font-bold">{{ userName }}</span>
           <span class="text-xs text-gray-400 font-semibold">09/20 22:37</span>
         </div>
+        <template v-if="curUser === userId">
+          <div class="flex flex-row divide-x divide-gray-400 items-center">
+            <div class="flex-none items-center">
+              <NuxtLink
+                to="/board/addarticle"
+                class="
+                  flex-none
+                  p-1
+                  text-sm
+                  rounded-md
+                  mr-4
+                  text-gray-400
+                  font-bold
+                  hover:bg-gray-200
+                "
+              >
+                수정
+              </NuxtLink>
+            </div>
+            <div class="flex-none items-center">
+              <button
+                class="
+                  flex-none
+                  p-1
+                  text-sm
+                  font-bold
+                  rounded-md
+                  text-gray-400
+                  hover:bg-gray-200
+                  ml-4
+                "
+                @click="deleteArticle"
+              >
+                삭제
+              </button>
+            </div>
+          </div>
+        </template>
       </div>
       <span class="flex flex-none text-base w-full my-8 text-gray-600">
-        언제 어디서 보나 달은 똑같은 달이에요. 다른건 달을 보는 사람의
-        마음이겠죠.<br />
-        제가 여기서 조금 힘듦으로 다른 사람들이 조금 더 편한 마음을 가지고 달을
-        볼 수 있겠죠. <br />
-        그렇게 생각하니 마음이 조금은 따뜻해지고 저의 군대에서의 시간이 조금 더
-        값지게 느껴지네요. <br />
-        이 간단한 걸 상꺾이 돼서야 깨닫네요. 간단한 걸 몰라서 그동안 혼자 그렇게
-        괴로워하고 이 시간들을 의심한 제가 미워지네요.<br /><br />
-        모두 좋은 추석되세요!
+        <pre class="font-sans"> {{ content }} </pre>
       </span>
     </div>
     <div class="w-full">
-      <span
-        class="
-          py-4
-          flex flex-none
-          w-full
-          text-2xl
-          items-start
-          text-gray-500
-          font-bold
-        "
-        >댓글 {{ commentIds.length }}</span
-      >
+      <div class="flex flex-row pt-4 gap-x-4 items-center">
+        <span class="flex text-2xl items-start text-gray-500 font-bold"
+          >댓글 {{ commentIds.length }}</span
+        >
+        <button
+          class="
+            flex flex-row
+            text-lg text-gray-400
+            px-2
+            py-1
+            rounded-md
+            items-center
+            group
+            hover:bg-gray-200
+          "
+          @click="toggle()"
+        >
+          <fa
+            :icon="['fas', 'plus-square']"
+            class="text-lg text-gray-400 mr-2 group-hover:bg-gray-200"
+          />
+          <template v-if="!isCommentMode"> 댓글 추가 </template>
+          <template v-else> 댓글 취소 </template>
+        </button>
+      </div>
+      <template v-if="isCommentMode">
+        <AddComment
+          :datas="{
+            userId: 3,
+            userName: '익명 (글쓴이)',
+            isWriter: true,
+            parentCommentId: -1
+          }"
+        />
+      </template>
       <ul class="flex flex-col w-240 items-start">
         <li v-for="(commentId, index) in commentIds" :key="index">
           <Comment
-            :comment-id="commentId"
-            :test-data="testDatas[index]"
+            :id="commentId"
+            :datas="commentDatas[index]"
             :is-even="index % 2 != 0"
             class="my-1"
           />
@@ -98,39 +175,88 @@
 export default {
   data() {
     return {
+      curUser: 3,
+      boardId: -1,
+      boardName: '',
+      title: '',
+      userId: -1,
+      userName: '',
+      upVote: 0,
+      downVote: 0,
+      vote: 0,
+      content: ``,
       commentIds: [1, 2, 3],
-      testDatas: [
+      commentDatas: [
         {
           userId: 1,
           userName: `익명1`,
-          content: `나라 지킨다고 고생 많으세요:) 감사합니다!`,
-          vote: 3,
-          isCcc: false
+          content: `나라 지킨다고 고생 많으세요:)
+감사합니다!`,
+          upVote: 5,
+          downVote: 2,
+          parentCommentId: -1
         },
         {
           userId: 2,
           userName: `익명2`,
           content: `빨리 전역해라 힘내고`,
-          vote: 1,
-          isCcc: false
+          upVote: 4,
+          downVote: 3,
+          parentCommentId: -1
         },
         {
           userId: 3,
           userName: `익명 (글쓴이)`,
           content: `감사합니다ㅠ`,
-          vote: 1,
-          isCcc: true
+          upVote: 1,
+          downVote: 0,
+          parentCommentId: 2
         }
-      ]
+      ],
+      isCommentMode: false,
+      isWriter: false
     }
   },
+  async fetch() {
+    this.boardId = await new Promise((resolve) => resolve())
+    this.boardName = await new Promise((resolve) => resolve())
+    this.title = await new Promise((resolve) => resolve())
+    this.userId = await new Promise((resolve) => resolve())
+    this.userName = await new Promise((resolve) => resolve())
+    this.content = await new Promise((resolve) => resolve())
+    this.vote = await new Promise((resolve) => resolve())
+    this.upVote = await new Promise((resolve) => resolve())
+    this.downVote = await new Promise((resolve) => resolve())
+
+    this.boardId = 1
+    this.boardName = '자유 게시판'
+    this.title = '군대에서 근무서면서 달을 보는데 정말 환하더군요'
+    this.userId = 3
+    this.userName = '익명'
+    this.content = `언제 어디서 보나 달은 똑같은 달이에요. 다른건 달을 보는 사람의 마음이겠죠.
+제가 여기서 조금 힘듦으로 다른 사람들이 조금 더 편한 마음을 가지고 달을 볼 수 있겠죠.
+그렇게 생각하니 마음이 조금은 따뜻해지고 저의 군대에서의 시간이 조금 더 값지게 느껴지네요.
+이 간단한 걸 상꺾이 돼서야 깨닫네요. 간단한 걸 몰라서 그동안 혼자 그렇게 괴로워하고 이 시간들을 의심한 제가 미워지네요.
+
+모두 좋은 추석되세요!`
+    this.upVote = 95
+    this.downVote = 40
+    this.vote = this.upVote - this.downVote
+    this.isWriter = true
+  },
   methods: {
-    upVote() {
+    increaseUpVote() {
       this.vote++
+      this.upVote++
     },
-    downVote() {
+    increaseDownVote() {
       this.vote--
-    }
+      this.downVote++
+    },
+    toggle() {
+      this.isCommentMode = !this.isCommentMode
+    },
+    deleteArticle() {}
   }
 }
 </script>
--- frontend/components/Comment.vue
+++ frontend/components/Comment.vue
@@ -1,66 +1,141 @@
 <template>
-  <div :class="isCcc ? 'ml-12 w-216' : 'w-216'">
-    <div
-      class="w-full flex-col p-4 items-start"
-      :class="isEven ? 'bg-gray-100 rounded-lg' : ''"
-    >
-      <div class="flex flex-row w-full justify-between space-x-2">
-        <div class="flex flex-none w-6 h-6 rounded-md items-center bg-gray-300">
-          <fa
-            :icon="['fas', 'user']"
+  <div v-if="!isModifyingMode">
+    <div :class="datas.parentCommentId != -1 ? 'ml-12 w-204' : 'w-216'">
+      <div
+        class="w-full flex-col p-4 pb-3 items-start"
+        :class="isEven ? 'bg-gray-100 rounded-lg' : ''"
+      >
+        <div class="flex flex-row w-full justify-between space-x-2">
+          <div
+            class="flex flex-none w-6 h-6 rounded-md items-center bg-gray-300"
+          >
+            <fa
+              :icon="['fas', 'user']"
+              class="
+                flex
+                text-base
+                w-full
+                m-auto
+                items-center
+                text-white
+                bg-gray-300
+              "
+            />
+          </div>
+          <div
+            class="flex flex-none w-full text-sm font-bold items-center"
+            :class="isWriter ? 'text-military' : 'text-gray-500'"
+          >
+            {{ datas.userName }}
+            <template v-if="curUser === datas.userId"
+              ><button
+                class="
+                  flex
+                  p-1
+                  text-sm
+                  rounded-md
+                  text-gray-400
+                  ml-4
+                  mr-2
+                  hover:bg-gray-200
+                "
+                @click="toggleModifyingMode"
+              >
+                수정
+              </button>
+              <button
+                class="
+                  flex
+                  p-1
+                  text-sm
+                  rounded-md
+                  text-gray-400
+                  hover:bg-gray-200
+                "
+                @click="deleteComment"
+              >
+                삭제
+              </button>
+            </template>
+          </div>
+        </div>
+        <span class="flex my-2 text-sm w-full text-gray-400 items-center">
+          <pre class="font-sans">{{ datas.content }}</pre>
+        </span>
+        <div class="flex flex-row justify-items-start items-center">
+          <button class="flex items-center" @click="increaseUpVote()">
+            <fa
+              :icon="['fas', 'caret-up']"
+              class="
+                flex
+                text-base text-gray-400
+                hover:drop-shadow hover:text-gray-600
+              "
+            />
+          </button>
+          <span class="flex text-sm text-gray-400 ml-2">
+            <template v-if="vote > 0">+{{ vote }}</template>
+            <template v-else>{{ vote }}</template>
+          </span>
+          <button class="flex items-center" @click="increaseDownVote()">
+            <fa
+              :icon="['fas', 'caret-down']"
+              class="
+                flex
+                text-base text-gray-400
+                ml-2
+                hover:drop-shadow hover:text-gray-600
+              "
+            />
+          </button>
+          <button
+            v-if="datas.parentCommentId == -1"
             class="
               flex
-              text-base
-              w-full
-              m-auto
-              items-center
-              text-white
-              bg-gray-300
+              p-1
+              text-sm
+              rounded-md
+              text-gray-400
+              ml-4
+              hover:bg-gray-200
             "
-          />
+            @click="toggleCommentMode"
+          >
+            <template v-if="!isCommentMode">답글</template>
+            <template v-else>답글 취소</template>
+          </button>
         </div>
-        <div
-          class="flex flex-none w-full text-sm font-bold items-center"
-          :class="isWriter ? 'text-military' : 'text-gray-500'"
-        >
-          {{ userName }}
-        </div>
-      </div>
-      <span
-        class="
-          flex
-          my-2
-          text-sm
-          w-full
-          text-gray-400
-          whitespace-nowrap
-          items-center
-        "
-      >
-        {{ content }}
-      </span>
-      <div class="flex flex-row justify-items-start items-center">
-        <fa :icon="['fas', 'caret-up']" class="flex text-base text-gray-400" />
-        <span class="flex text-sm text-gray-400 ml-2">+{{ vote }}</span>
-        <fa
-          :icon="['fas', 'caret-down']"
-          class="flex text-base text-gray-400 ml-2"
-        />
-        <Nuxtlink
-          v-if="!isCcc"
-          to="/board/:id/article/:article-id/답글"
-          class="flex text-sm text-gray-400 ml-4"
-          >답글</Nuxtlink
-        >
       </div>
     </div>
+    <template v-if="isCommentMode">
+      <AddComment
+        :datas="{
+          userId: 3,
+          userName: '익명 (글쓴이)',
+          isWriter: true,
+          parentCommentId: 2
+        }"
+    /></template>
+  </div>
+  <div v-else>
+    <AddComment
+      :datas="{
+        userId: datas.userId,
+        userName: datas.userName,
+        content: datas.content,
+        isWriter: isWriter,
+        isModifyingMode: isModifyingMode,
+        parentCommentId: datas.parentCommentId
+      }"
+      :toggle="toggleModifyingMode"
+    />
   </div>
 </template>
 
 <script>
 export default {
   props: {
-    commentId: {
+    id: {
       type: Number,
       default: -1
     },
@@ -68,54 +143,72 @@ export default {
       type: Boolean,
       default: false
     },
-    testData: {
+    datas: {
       type: Object,
       default: () => {
         return {
           userId: -1,
           userName: ``,
           content: ``,
-          vote: 0,
-          isCcc: false
+          upVote: 0,
+          downVote: 0,
+          parentCommentId: -1
         }
       }
     }
   },
   data() {
     return {
-      userId: -1,
-      userName: ``,
-      content: ``,
+      curUser: 3,
+      isModifyingMode: false,
+      upVote: 0,
+      downVote: 0,
       vote: 0,
-      isCcc: false,
-      isWriter: false
+      isWriter: false,
+      isCommentMode: false,
+      commentData: {
+        userId: -1,
+        userName: '',
+        isWriter: false,
+        parentCommentId: this.id
+      }
     }
   },
   async fetch() {
-    await this.getComment(this.commentId)
+    await this.fetchData()
   },
   methods: {
-    async getComment(commentId) {
-      this.userId = await new Promise((resolve) => resolve())
-      this.userName = await new Promise((resolve) => resolve())
-      this.content = await new Promise((resolve) => resolve())
-      this.vote = await new Promise((resolve) => resolve())
-      this.isCcc = await new Promise((resolve) => resolve())
+    async fetchData() {
       this.isWriter = await new Promise((resolve) => resolve())
+      this.vote = await new Promise((resolve) => resolve())
+      this.upVote = await new Promise((resolve) => resolve())
+      this.downVote = await new Promise((resolve) => resolve())
 
-      this.userId = this.testData.userId
-      this.userName = this.testData.userName
-      this.content = this.testData.content
-      this.vote = this.testData.vote
-      this.isCcc = this.testData.isCcc
-      this.isWriter = this.userName.includes('(글쓴이)')
-    }
-  },
-  upVote() {
-    this.vote++
-  },
-  downVote() {
-    this.vote--
+      this.upVote = this.datas.upVote
+      this.downVote = this.datas.downVote
+      this.vote = this.upVote - this.downVote
+      if (this.datas.userName.includes('(글쓴이)')) this.isWriter = true
+
+      this.commentData.userId = 3
+      this.commentData.userName = '익명 (글쓴이)'
+      if (this.commentData.userName.includes('(글쓴이)'))
+        this.commentData.isWriter = true
+    },
+    toggleCommentMode() {
+      this.isCommentMode = !this.isCommentMode
+    },
+    toggleModifyingMode() {
+      this.isModifyingMode = !this.isModifyingMode
+    },
+    increaseUpVote() {
+      this.vote++
+      this.upVote++
+    },
+    increaseDownVote() {
+      this.vote--
+      this.downVote++
+    },
+    deleteComment() {}
   }
 }
 </script>
