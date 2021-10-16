39245fa0f9d0ab859232af56682c5c715783796f
Kangmin Seo <kangrnin@gmail.com>
2021-10-16 00:46:52 -0900

    Add Timetable component

---
+++ frontend/components/Timetable.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    schedules: {
+      type: Array,
+      default: () => {
+        return [
+          {
+            name: '',
+            start_time: '',
+            end_time: '',
+            note: ''
+          }
+        ]
+      }
+    }
+  }
+}
+</script>
