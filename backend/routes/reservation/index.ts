af1da98c86708ab76367b1af5c1dc9188b858a9e
Jaemin Choi <1dotolee@gmail.com>
2021-10-16 13:49:43 +0000

    Implement facility CRUD API

--- backend/routes/reservation/index.ts
+++ backend/routes/reservation/index.ts
@@ -1,5 +1,6 @@
 import * as reservation from './reservation'
 import * as user from './user'
+import * as facility from './facility'
 import { authMiddleware } from '../../middleware/auth'
 import { Router } from 'express'
 
@@ -8,5 +9,9 @@ const router = Router()
 router.post('/', authMiddleware, reservation.post)
 router.delete('/', authMiddleware, reservation.delete_)
 router.get('/user', authMiddleware, user.get)
+router.get('/facility', authMiddleware, facility.get)
+router.post('/facility', authMiddleware, facility.post)
+router.patch('/facility', authMiddleware, facility.patch)
+router.delete('/facility', authMiddleware, facility.delete_)
 
 export default router
