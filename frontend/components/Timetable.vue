f5fea7f4b22395d081f042471fe12a664687694b
Kangmin Seo <kangrnin@gmail.com>
2021-10-18 07:32:42 -0900

    Render schedules on Timetable
    - 코드 정리 필요함

--- frontend/components/Timetable.vue
+++ frontend/components/Timetable.vue
@@ -3,22 +3,52 @@
     <div class="w-14 pr-4">
       <div class="mt-12">
         <div
-          v-for="(n, hourIdx) in durationHours+1"
+          v-for="(n, hourIdx) in durationHours + 1"
           :key="hourIdx"
           class="h-14 text-right text-gray-500 text-lg font-bold"
         >
-          {{ start.getHours() + hourIdx }}시
+          {{ parseTime(start).getHours() + hourIdx }}시
         </div>
       </div>
     </div>
     <div v-for="(date, dateIdx) in dates" :key="dateIdx" class="w-40">
-      <span class="block text-lg text-center font-bold"
-        :class="dateIdx == 0 ? 'text-red-500' : dateIdx == 6 ? 'text-blue-500' : 'text-gray-500'">
+      <span
+        class="block text-lg text-center font-bold"
+        :class="
+          date.getDay() == 0
+            ? 'text-red-500'
+            : date.getDay() == 6
+            ? 'text-blue-500'
+            : 'text-gray-500'
+        "
+      >
         {{
           `${date.getMonth() + 1}/${date.getDate()} (${korDays[date.getDay()]})`
         }}
       </span>
-      <div class="mt-8" :class="dateIdx != 0 && 'border-l-2 border-gray-500'">
+      <div
+        class="mt-8 relative"
+        :class="dateIdx != 0 && 'border-l-2 border-gray-500'"
+      >
+        <div v-for="schedule in schedules" :key="schedule.id">
+          <div
+            v-if="date.getDate() === new Date(schedule.date).getDate()"
+            class="
+              absolute
+              w-full
+              bg-green-400
+              rounded-xl
+              text-white text-lg text-center
+            "
+            :style="`top: ${parseInt(
+              getHourDifference(parseTime(schedule.start_time), parseTime(start)) * 56
+            )}px; height:${parseInt(
+              getHourDifference(parseTime(schedule.end_time), parseTime(schedule.start_time)) * 56
+            )}px;`"
+          >
+            {{ schedule.name ? schedule.name : schedule.note }}
+          </div>
+        </div>
         <div v-for="(n, hourIdx) in durationHours" :key="hourIdx" class="h-14">
           <div
             class="mx-2 h-14 border-t border-gray-300"
@@ -33,16 +63,23 @@
 <script>
 export default {
   props: {
-    start: Date,
-    end: Date,
+    start: {
+      type: String,
+      default: '09:00'
+    },
+    end: {
+      type: String,
+      default: '24:00'
+    },
     schedules: {
       type: Array,
       default: () => {
         return [
           {
+            date: '',
             name: '',
-            start_time: '',
-            end_time: '',
+            start_time: new Date(),
+            end_time: new Date(),
             note: ''
           }
         ]
@@ -58,15 +95,13 @@ export default {
   computed: {
     durationHours() {
       return (
-        this.end.getHours() +
-        (this.end.getMinutes() > 0 && 1) -
-        this.start.getHours()
+        this.parseTime(this.end).getHours() +
+        (this.parseTime(this.end).getMinutes() > 0 && 1) -
+        this.parseTime(this.start).getHours()
       )
     }
   },
   mounted() {
-    alert(this.start)
-    alert(this.end)
     this.dates.push(new Date())
     for (let i = 0; i < 6; i++) {
       const nextDate = new Date(this.dates[this.dates.length - 1].getTime())
@@ -75,6 +110,20 @@ export default {
     }
   },
   methods: {
+    getHourDifference(t1, t2) {
+      return (
+        t1.getHours() +
+        t1.getMinutes() / 60 -
+        (t2.getHours() + t2.getMinutes() / 60)
+      )
+    },
+    parseTime(time) {
+      const temp = time.split(':')
+      const date = new Date()
+      date.setHours(temp[0])
+      date.setMinutes(temp[1])
+      return date
+    }
   }
 }
 </script>
